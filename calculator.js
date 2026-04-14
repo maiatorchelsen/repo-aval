f3a78f86e1fdae2c5e2dfc55003d56d09070f39b
Brian da Silva Guterres <briansguterres@hotmail.com>
2026-04-14 10:20:18 -0300

    ✨ Adicionada a função de divisão

--- calculator.js
+++ calculator.js
@@ -5,7 +5,12 @@ function sum(v1, v2) {}
 function sub(v1, v2) {}
 
 // retorna o valor da divisão entre os dois parâmetros
-function div(v1, v2) {}
+function div(v1, v2) {
+  if (v2 === 0) {
+    throw new Error("Não é possível dividir por zero.");
+  }
+  return v1 / v2;
+}
 
 // retorna o valor da multiplicação entre os dois parâmetros
 function mult(v1, v2) {}
@@ -13,4 +18,4 @@ function mult(v1, v2) {}
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {}
 
-console.log(sum(v1, v2));
+console.log(div(10, 2)); // Exemplo de uso da função div
